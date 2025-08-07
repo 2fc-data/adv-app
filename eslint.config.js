b1ee8a502e3520fc3273926fa8705e20a6474532
Fernando <camargo.fernando@gmail.com>
2025-08-07 09:16:23 -0300

    primeiros ajustes

--- eslint.config.js
+++ eslint.config.js
@@ -1,46 +1,23 @@
-import type { Linter } from 'eslint'
 import js from '@eslint/js'
-import { globals: globalVariables } from 'globals'
+import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import ts from '@typescript-eslint/parser'
-import { config as tsConfig } from '@typescript-eslint/eslint-plugin'
+import tseslint from 'typescript-eslint'
+import { globalIgnores } from 'eslint/config'
 
-export default [
-  { ignores: ['dist'] },
+export default tseslint.config([
+  globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      parser: ts,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        project: './tsconfig.json',
-        sourceType: 'module',
-        warnOnUnsupportedTypeScriptVersion: false,
-      },
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...js.configs.recommended.rules,
-      ...tsConfig.rules,
-      ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-    overrides: [
-      {
-        files: ['**/*.ts'],
-        rules: {
-          'no-shadow': 'off',
-        },
-      },
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
     ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
   },
-] as const satisfies Linter.Config[]
-
+])
